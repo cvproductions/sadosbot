42d09e1d54986e28ea899ba3797d15a8bedfb899
cvproductions <38675635+cvproductions@users.noreply.github.com>
2018-06-19 23:49:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
-const tokenfile = require("./token.json");
-const fs = require("fs");
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
-
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("bot things...", {type: "PLAYING"});
+ console.log(${bot.user.username} is online on ${bot.guilds.size} servers!);
+ bot.user.setActivity("bot things...", {type: "PLAYING"});
 
 });
 
 bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+ if(message.author.bot) return;
+ if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+ let prefix = botconfig.prefix;
+ let messageArray = message.content.split(" ");
+ let cmd = messageArray[0];
+ let args = messageArray.slice(1);
+ let commandfile = bot.commands.get(cmd.slice(prefix.length));
+ if(commandfile) commandfile.run(bot,message,args);
 
 });
 
-
-var cli = new Discord.Client({autoReconnect:true});
-
-
-bot.login(tokenfile.token);
+client.login(process.env.BOT_TOKEN);
